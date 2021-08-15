4d5a6e4bd19e102289696540c1c44d920fa716a6
Atsushi.M <>
2021-08-15 11:57:31 +0900

    change key name in json

--- api/models/user.go
+++ api/models/user.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type User struct {
-	ID        uint `gorm:"primary_key"`
-	Name      string
-	Age       int
-	Height    *int
-	Weight    *int
-	UpdatedAt time.Time
-	CreatedAt time.Time
+	ID        uint      `json:"id" gorm:"primary_key"`
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+	Height    *int      `json:"height"`
+	Weight    *int      `json:"weight"`
+	UpdatedAt time.Time `json:"-"`
+	CreatedAt time.Time `json:"-"`
 }
